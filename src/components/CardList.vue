790ac4f76fb2ba1f074e4d0400b402837a406322
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-11 15:16:53 +0200

    feat: add correct card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,6 +2,7 @@
     import SingleCard from './SingleCard.vue';
       // importare lo store
     import {store} from '../store';
+import { info } from 'sass';
         export default {
         name: 'CardList',
         components : {
@@ -19,7 +20,7 @@
     <section class="container">
         <div class="row">
             <div v-for="card in store.cardDeck" :key="card.id" class="col-6 col-md-4 col-lg-2 mb-5">
-                <SingleCard/>
+                <SingleCard :info="card"/>
             </div>
             <!-- schermata contenente le carte -->
         </div>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,12 +1,15 @@
 <script>
+import { info } from 'sass';
+
 export default {
     name : 'SingleCard',
+    props: ['info'],
 }
 </script>
 
 <template>
     <div class="character text-center">
-        <img src="https://m.media-amazon.com/images/I/81X0qR7QzIL._AC_UF1000,1000_QL80_.jpg" alt="">
+        <img :src="info.card_images[0].image_url">
         <h5>NOME DELLA CARTA</h5>
         <h6>Specie</h6>
     </div>
