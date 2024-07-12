8295551f01e6b7a982fd8949a46c12113916b6be
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-12 13:45:32 +0200

    feat: change name and number of cards

--- src/store.js
+++ src/store.js
@@ -2,5 +2,5 @@ import { reactive } from "vue"
 
 export const store = reactive({
     cardDeck: [],
-    apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0',
+    apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=25&offset=0',
 });
